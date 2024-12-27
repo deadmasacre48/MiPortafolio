b4dfc4cd3c1b905affd5d8a135d4cc29b19fff71
deadmasacre48 <elcapomax48@gmail.com>
2024-12-27 00:28:27 -0400

    Ultimo agregado a lo mejor

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import { createRoot } from "react-dom/client";
-import React from "react";
 import "./index.css";
+
 import App from "./App.jsx";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
